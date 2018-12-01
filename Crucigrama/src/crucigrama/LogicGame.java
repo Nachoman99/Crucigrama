ea1bdbc0405755ef14c999a22396caa942d49d08
Edwin David <Edwin David@192.168.1.3>
2018-12-01 17:56:12 -0600

    Documentacion y paso de los metodos a el LogicGame

--- Crucigrama/src/crucigrama/LogicGame.java
+++ Crucigrama/src/crucigrama/LogicGame.java
@@ -10,5 +10,62 @@ package crucigrama;
  * @author Nacho
  */
 public class LogicGame {
-    
+     /**
+     * public Letter[] stringToLetter(String extraction)
+     * This method converts the extraction string to a 
+     * array Letter
+     * @param extraction
+     * @return return the array of Letter
+     */
+    public Letter[] stringToLetter(String extraction){
+        Letter[] palabra = new Letter[extraction.length()];
+        for (int i = 0; i < extraction.length(); i++) {
+            char intemedio = extraction.charAt(i);
+            Letter letra = new Letter(intemedio);
+            palabra[i]=(letra); 
+        }
+        return palabra;
+    }
+    /**
+     * public String extraction(int filaInicial,int columnaInicial, char verticalHorizontal)
+     * This method extracts a word from the crossword
+     * @param filaInicial the 
+     * @param columnaInicial
+     * @param verticalHorizontal
+     * @return return the extract word
+     */
+    public String extraction(int filaInicial,int columnaInicial, char verticalHorizontal, Crossword crossword){
+        String palabra = "";
+        Letter letter = new Letter('0');
+        if(verticalHorizontal == 'V'){
+            for (int i = columnaInicial; i < crossword.columnLength(); i++) {
+                if(crossword.getChar(filaInicial, i) != letter.getLetter()){
+                    palabra += crossword.getChar(filaInicial, i);
+                }
+            }
+        }else if(verticalHorizontal == 'H'){
+            for (int i = filaInicial; i < crossword.rowlength(); i++) {
+                if(crossword.getChar(i, columnaInicial) != letter.getLetter()){
+                    palabra += crossword.getChar(i, columnaInicial);
+                }
+            }
+        }
+
+        return palabra;
+    }
+      private int validar(int filas, Letter[] letra, Letter[] crossword, int letrasMalas){
+        if (filas > letra.length-1) {
+           // System.out.println(letrasMalas + 1);
+            return letrasMalas;
+        } else {
+            if (letra[filas] != crossword[filas]) {
+                letrasMalas += 1;
+               // System.out.println("Hola");
+            }
+            return validar(filas + 1, letra, crossword, letrasMalas);
+        }
+    }
+       public int validar(Letter[] letra, Letter[] palabraCorrecta){
+        return validar(0, letra, palabraCorrecta, 0);
+    }
 }
