8291033a64782a1ab3159ad7faea68677a591257
Kevin Trejos <Kevin Trejos@192.168.1.5>
2018-12-02 16:55:00 -0600

    Word and WordList

--- Crucigrama/src/crucigrama/WordList.java
+++ Crucigrama/src/crucigrama/WordList.java
@@ -10,27 +10,35 @@ package crucigrama;
  * @author Kevin Trejos
  */
 public class WordList {
-    private Word wordList;
+    private Word[] wordList;
+    private int counter;
 
     public WordList() {
     }
 
-    public WordList(Word wordList) {
+    public WordList(Word[] wordList, int counter) {
         this.wordList = wordList;
+        this.counter = counter;
     }
 
-    public Word getWordList() {
+    public Word[] getWordList() {
         return wordList;
     }
 
-    public void setWordList(Word wordList) {
+    public void setWordList(Word[] wordList) {
         this.wordList = wordList;
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
     }
 
     @Override
     public String toString() {
-        return "WordList{" + "wordList=" + wordList + '}';
+        return "WordList{" + "wordList=" + wordList + ", counter=" + counter + '}';
     }
-    
-    
 }
