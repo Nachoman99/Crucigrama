543cdcdf74c4935508f5edfebe72defa8f8f93a6
Kevin Trejos <Kevin Trejos@192.168.1.5>
2018-12-05 16:47:22 -0600

    Txt

--- Crucigrama/src/crucigrama/GUI/GameWindow.java
+++ Crucigrama/src/crucigrama/GUI/GameWindow.java
@@ -79,7 +79,7 @@ public class GameWindow extends javax.swing.JDialog {
         System.out.println(Game.WORD_LIST_MANAGER.getEspecificWord(4));
         initPanel(rowColumn ,crossword1);
     }
-
+ 
     /**
      * This method is called from within the constructor to initialize the form.
      * WARNING: Do NOT modify this code. The content of this method is always
